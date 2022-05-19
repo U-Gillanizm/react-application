40a64a5e579cadb81f64385ebcc5d1f2c6d22dae
Usama Gillani <usamagillani91@gmail.com>
2022-05-19 19:41:08 +0500

    Update main.yml

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Usama Gillani is accessible
+          Usama Gillani is accessible to everyone
         </p>
         <a
           className="App-link"
